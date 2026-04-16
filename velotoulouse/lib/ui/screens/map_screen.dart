c0e5bf2dbc9c6ad8e25f3ceb5db512579fe27578
taingchhaysophal <filmakame@gmail.com>
2026-04-16 18:36:43 +0700

    Map Screen

--- velotoulouse/lib/ui/screens/map_screen.dart
+++ velotoulouse/lib/ui/screens/map_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(43.6047, 1.4442), // Toulouse city center
+          initialZoom: 14,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.velotoulouse',
+          ),
+        ],
+      )
+    );
   }
 }
