04f6eb69e453a749e9201a7e37792bb1938a169e
Samnang-Vouen <vouensamnang@gmail.com>
2026-04-20 20:37:28 +0700

    Add AsyncValue Utility for Managing Asynchronous State. Close #19

---
+++ velotoulouse/lib/ui/utils/async_value.dart
@@ -0,0 +1,21 @@
+enum AsyncValueState { loading, error, success }
+
+class AsyncValue<T> {
+  final T? data;
+  final Object? error;
+  final AsyncValueState state;
+
+  AsyncValue._({this.data, this.error, required this.state});
+
+  factory AsyncValue.loading() => AsyncValue._(state: AsyncValueState.loading);
+
+  factory AsyncValue.success(T data) =>
+      AsyncValue._(data: data, state: AsyncValueState.success);
+
+  factory AsyncValue.error(Object error) =>
+      AsyncValue._(error: error, state: AsyncValueState.error);
+
+  bool get isLoading => state == AsyncValueState.loading;
+  bool get isSuccess => state == AsyncValueState.success;
+  bool get isError => state == AsyncValueState.error;
+}
