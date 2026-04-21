5d2abe10300cd15f5566afd370826a11b8246717
taingchhaysophal <filmakame@gmail.com>
2026-04-21 19:16:09 +0700

    map screen

--- velotoulouse/lib/ui/screens/map/map_screen.dart
+++ velotoulouse/lib/ui/screens/map/map_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:velotoulouse/data/repositories/station/station_repository_real.dart';
 import 'package:velotoulouse/ui/screens/map/view_model/map_view_model.dart';
 import 'package:velotoulouse/ui/screens/map/widgets/map_content.dart';
+import 'package:velotoulouse/ui/states/user_state.dart';
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
@@ -11,6 +12,7 @@ class MapScreen extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MapViewModel(
         stationRepository: context.read<StationRepository>(),
+        userState: context.read<UserState>(),
       ),
       child: const MapContent(),
     );
