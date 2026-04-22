fec0a97ce7b35e56372aebec8aca7ede7e5ce5fd
taingchhaysophal <filmakame@gmail.com>
2026-04-22 16:01:58 +0700

    improve map

--- velotoulouse/lib/ui/screens/map/map_screen.dart
+++ velotoulouse/lib/ui/screens/map/map_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:velotoulouse/data/repositories/station/station_repository.dart';
-import 'package:velotoulouse/data/repositories/station/station_repository_real.dart';
 import 'package:velotoulouse/ui/screens/map/view_model/map_view_model.dart';
 import 'package:velotoulouse/ui/screens/map/widgets/map_content.dart';
 import 'package:velotoulouse/ui/states/user_state.dart';
